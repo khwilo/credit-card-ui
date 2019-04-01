5b68bda5d8a1c481dd9f747f645fa99d42c6ab43
khwilo <khwilowatai@gmail.com>
2019-04-01 13:45:32 +0300

    Add a cardInfo as a prop to the CreditCard component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CreditCard from './components/CreditCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cardInfo = {
+    person        : {
+        firstName: "CardHolder",
+        lastName : "Name"
+    },
+    expirationDate: "08/19",
+    cardNumber    : "1234 5678 8765 4321",
+    bankName      : "Big Bank, Inc"
+};
+
+ReactDOM.render(<CreditCard cardInfo={cardInfo} />, document.getElementById('root'));
